64cdf0419e7c6a7cbbbffec37b7b14c54e75c589
Ryosuke Hayashi <hayashi@hayashikun.com>
2021-02-14 19:47:43 +0900

    First commit

---
+++ src/main.rs
@@ -0,0 +1,20 @@
+use actix_web::{App, get, HttpServer, Responder, web};
+
+#[get("/")]
+async fn index() -> impl Responder {
+    format!("Hello world")
+}
+
+
+#[get("/{name}/greet")]
+async fn name_index(web::Path((name, )): web::Path<(String, )>) -> impl Responder {
+    format!("Hello {}!", name)
+}
+
+#[actix_web::main]
+async fn main() -> std::io::Result<()> {
+    HttpServer::new(|| App::new().service(index).service(name_index))
+        .bind("127.0.0.1:8080")?
+        .run()
+        .await
+}
