376d90b6266838edb366f9976e6a6f42a06ae54b
Ryosuke Hayashi <hayashi@hayashikun.com>
2021-02-15 22:03:43 +0900

    enable to start http2 nginx server

--- server/src/main.rs
+++ server/src/main.rs
@@ -14,7 +14,7 @@ async fn name_index(web::Path((name, )): web::Path<(String, )>) -> impl Responde
 #[actix_web::main]
 async fn main() -> std::io::Result<()> {
     HttpServer::new(|| App::new().service(index).service(name_index))
-        .bind("127.0.0.1:8080")?
+        .bind("0.0.0.0:8080")?
         .run()
         .await
 }
