e2678cf1dbae9c796fc5cf10377f387da2d051a1
Ryosuke Hayashi <hayashi@hayashikun.com>
2021-02-16 23:04:02 +0900

    work!

--- server/src/main.rs
+++ server/src/main.rs
@@ -1,20 +1,30 @@
-use actix_web::{App, get, HttpServer, Responder, web};
+use actix_web::{App, error, Error, get, HttpResponse, HttpServer, web};
+use tera::{Context, Tera};
+
+use actix_files as fs;
 
 #[get("/")]
-async fn index() -> impl Responder {
-    format!("Hello world")
+async fn index(tmpl: web::Data<Tera>) -> Result<HttpResponse, Error> {
+    let ctx = Context::new();
+    let view = tmpl
+        .render("index.html", &ctx)
+        .map_err(|e| error::ErrorInternalServerError(e))?;
+
+    Ok(HttpResponse::Ok().content_type("text/html").body(view))
 }
 
-
-#[get("/{name}/greet")]
-async fn name_index(web::Path((name, )): web::Path<(String, )>) -> impl Responder {
-    format!("Hello {}!", name)
-}
 
 #[actix_web::main]
 async fn main() -> std::io::Result<()> {
-    HttpServer::new(|| App::new().service(index).service(name_index))
-        .bind("0.0.0.0:8080")?
+    let views = Tera::new("view/**/*").unwrap();
+
+    HttpServer::new(move ||
+        {
+            App::new()
+                .service(index)
+                .service(fs::Files::new("/static", "static").show_files_listing())
+                .data(views.clone())
+        }).bind("0.0.0.0:8080")?
         .run()
         .await
 }
